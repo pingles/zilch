1e59319b6b05869694fbb7367c4c389c46085b97
Tim Dysinger <tim@dysinger.net>
2010-09-29 14:17:40 -0400

    Changed library name

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject zilch "0.1.0-SNAPSHOT"
   :description "Conch: A Clojure 0MQ Messaging Library"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.zeromq/jzmq "2.0.9"]]
+                 [org.zeromq/zmq "2.0.9"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :native-path "/usr/local/lib:/opt/local/lib:/usr/lib"
   :namespaces [zilch.mq

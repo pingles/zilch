afed1de819973d0b692caf56092ee6fff0fe39cc
Tim Dysinger <tim@dysinger.net>
2010-10-03 07:18:38 -1000

    Corrected project name

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject zilch "0.1.0-SNAPSHOT"
-  :description "Conch: A Clojure 0MQ Messaging Library"
+  :description "Zilch: A Clojure 0MQ Messaging Library"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.zeromq/zmq "2.0.9"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]

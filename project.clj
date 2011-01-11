29059263c391d2fb35afa206a194cddc778b519e
Tim Dysinger <tim@dysinger.net>
2011-01-11 09:49:47 -0800

    Changed dep versions

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject zilch "0.1.0-SNAPSHOT"
   :description "Zilch: A Clojure 0MQ Messaging Library"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.zeromq/zmq "2.0.10"]]
+                 [org.zeromq/zmq "2.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :native-path "/usr/local/lib:/opt/local/lib:/usr/lib"
   :namespaces [zilch.mq
